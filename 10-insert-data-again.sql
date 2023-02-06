d117cdd9795740c1a170d0d59f92b7a4f8d1c2d8
123ilya <123ilya@gmail.com>
2023-02-06 23:08:42 +1000

    update table sql

---
+++ 10-insert-data-again.sql
@@ -0,0 +1,11 @@
+-- INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring) 
+-- VALUES ('Learning Inc', 'Educationstreet 12, London', 0.87,TRUE);
+
+-- INSERT INTO employers (company_name, company_address, yearly_revenue) 
+-- VALUES ('Big Oil', 'Slipperystreet 110, Houston', 112.55);
+
+-- INSERT INTO employers (company_name, company_address, yearly_revenue, is_hiring) 
+-- VALUES ('Hipster Food', 'Avocadostreet 5, Berlin', 6.12,TRUE);
+
+INSERT INTO conversations (user_name, employer_name, message)
+VALUES ('Max Schwartz', 'Learning Inc', 'Hi! I Like learning!'  );
